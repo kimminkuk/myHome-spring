deae54aaed523e8ffec6383addc7fb1071eabfa3
kimminkuk <rlaal567@gmail.com>
2022-07-14 23:07:06 +0900

    Init Setting... (15%) Study TT

---
+++ src/main/java/com/example/myHome/myHomespring/service/MemberService.java
@@ -0,0 +1,37 @@
+package com.example.myHome.myHomespring.service;
+
+import com.example.myHome.myHomespring.domain.Member;
+import com.example.myHome.myHomespring.repository.MemberRepository;
+import com.example.myHome.myHomespring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m ->{
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
