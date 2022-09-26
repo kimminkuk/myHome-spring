c8c980a7ca0ba75abb53536fd2455d4dbef6b910
kimminkuk <rlaal567@gmail.com>
2022-09-26 23:15:39 +0900

    Game Page (95%) Redis 조회해서 Ranking Page에 등록 완료. ( 몇 순위까지 설정, 정렬 등을 아직 대기 )

--- src/main/java/com/example/myHome/myHomespring/controller/gamePageController.java
+++ src/main/java/com/example/myHome/myHomespring/controller/gamePageController.java
@@ -36,8 +36,13 @@ public class gamePageController {
                                    @RequestParam("name") String name,
                                    @RequestParam("score") String score) {
 
-        String nowTime = getNowTime() + "@@" + score;
-        RedisMember resultMember = new RedisMember(name, nowTime);
+        //TODO: 결과 데이터에 날짜 시간을 추가하니 정렬이 매우 힘들었다.
+        // 제거하도록한다. 그리고 순수한 int 값만 넣도록 한다.
+
+        //String nowTime = getNowTime() + "@@" + score;
+        //RedisMember resultMember = new RedisMember(name, nowTime);
+        RedisMember resultMember = new RedisMember(name, score);
+
         redisGameResultDataService.join(resultMember);
         return "game/game.html";
     }
--- src/main/java/com/example/myHome/myHomespring/controller/gameRankPageController.java
+++ src/main/java/com/example/myHome/myHomespring/controller/gameRankPageController.java
@@ -20,15 +20,19 @@ public class gameRankPageController {
     }
 
     //전체 랭킹 조회
-    @GetMapping("/game/rank-page")
+    @GetMapping(value = "/game/rank-page")
     public String myHomeGameRankPage(Model model) {
         // TODO:
         // 랭킹은 1000위까지 보여주는걸로 redisService를 추가 코딩할거임
-        //List<RedisMember> members = redisRankPageService.findMembers();
-        //model.addAttribute("members", members);
+        List<RedisMember> members = redisRankPageService.findMembers();
+        model.addAttribute("members", members);
         /**
          *
-         * 2022-09-25 22:59:13.202 ERROR 58268 --- [nio-8080-exec-2] o.a.c.c.C.[.[.[/].[dispatcherServlet]    : Servlet.service() for servlet [dispatcherServlet] in context with path [] threw exception [Request processing failed; nested exception is org.thymeleaf.exceptions.TemplateProcessingException: Exception evaluating SpringEL expression: "members.key" (template: "game/rank-page.html" - line 20, col 25)] with root cause
+         * 2022-09-25 22:59:13.202 ERROR 58268 --- [nio-8080-exec-2] o.a.c.c.C.[.[.[/].[dispatcherServlet]
+         * : Servlet.service() for servlet [dispatcherServlet] in context with path []
+         * threw exception [Request processing failed; nested exception is org.thymeleaf.exceptions.
+         * TemplateProcessingException: Exception evaluating SpringEL expression:
+         * "members.key" (template: "game/rank-page.html" - line 20, col 25)] with root cause
          *
          * org.springframework.expression.spel.SpelEvaluationException: EL1008E: Property or field 'key' cannot be found on object of type 'java.util.ArrayList' - maybe not public or not valid?
          *
