883b6f4c3ab0644c1b55c01f2717b01196e4c303
kimminkuk <rlaal567@gmail.com>
2022-10-12 23:28:14 +0900

    css 조정 중 (15%) 더 보기 버튼 추가 중..

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -58,3 +58,56 @@ function goRankPageHtml() {
     xhr.send();
     return;
 }
+
+function btnLight() {
+    let liBtnList = document.querySelectorAll(".li-menu-btn");
+    liBtnListLen = liBtnList.length;
+    for (let i = 0; i < liBtnListLen; i++) {
+        //liBtnList 버튼에 마우스를 올려두면 색이 변하게 해줘
+        liBtnList[i].addEventListener("mouseover", function() {
+            //연한 회색으로
+            liBtnList[i].style.backgroundColor = "#e6e6e6";
+        });
+
+        //liBtnList 버튼이 마우스를 떠나면 색이 변하게 해줘
+        liBtnList[i].addEventListener("mouseout", function() {
+            //흰색으로
+            liBtnList[i].style.backgroundColor = "#ffffff";
+        });        
+    }
+    return;
+}
+
+// 더보기 버튼을 누르면 추가 메뉴가 나오게 해줘
+function gamePageMoreList() {
+    let btnMore = document.querySelector(".btn-more");
+    let ulMore = document.querySelector(".ul-more");
+    btnMore.addEventListener("click", function(){
+        if (ulMore.style.display === "none") {
+            ulMore.style.display = "block";
+        } else {
+            ulMore.style.display = "none";
+        }
+    });
+    //liMore 변수에 class 이름은 btn-more > ul > li 설정해줘
+    // let liMore = document.querySelectorAll(".btn-more > ul > li");
+    
+    // btnMore.addEventListener("click", function() {
+    //     if (liMore.style.display === "none") {
+    //         liMore.style.display = "block";
+    //     } else {
+    //         liMore.style.display = "none";
+    //     }
+    // });
+    return;
+}
+
+// 서버가 실행하면 바로 실행하게 해줘
+
+var ulMore = document.querySelector(".btn-more > ul");
+ulMore.style.display = "none";
+    // var liMore = document.querySelectorAll(".btn-more > ul >li");
+    // var liMoreLen = liMore.length;
+    // for (let i = 0; i < liMoreLen; i++) {
+    //     liMore[i].style.display = "none";
+    // }
